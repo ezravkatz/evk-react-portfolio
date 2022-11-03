d2349e4d15effb50c2d3ebb5f67934975916d07e
ezravkatz <ezravkatz@gmail.com>
2022-11-03 19:45:52 -0400

    general stylesheet

---
+++ src/components/experience/Experience.jsx
@@ -0,0 +1,13 @@
+import './experience.css'
+
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div>
+      Experience
+    </div>
+  )
+}
+
+export default Experience
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,13 @@
+import './testimonials.css'
+
+import React from 'react'
+
+const Testimonials = () => {
+  return (
+    <div>
+      Testimonials
+    </div>
+  )
+}
+
+export default Testimonials
