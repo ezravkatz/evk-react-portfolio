b1c57bd532ad9fed12f29666688a78073d9f2405
ezravkatz <ezravkatz@gmail.com>
2022-11-03 22:04:42 -0400

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,26 @@
-import './header.css'
-
 import React from 'react'
+import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me2.jpg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>
-      Header
-    </div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I am</h5>
+        <h1>Ezra Katz</h1>
+        <h5 className='text-light'>Front-end Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="A photograph of Ezra Katz in a museum." />
+        </div>
+
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
+      </div>
+    </header>
   )
 }
 
