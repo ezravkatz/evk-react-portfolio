02361b0ee6f75627400fc82675755c8ba4d4313c
ezravkatz <ezravkatz@gmail.com>
2022-11-04 01:24:41 -0400

    services component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,11 +1,111 @@
-import './services.css'
-
 import React from 'react'
+import './services.css'
+import {HiBadgeCheck} from 'react-icons/hi'
 
 const Services = () => {
   return (
     <section id='services'>
-      Services
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+        <div className="service__head">
+        <h3>UI/UX Design</h3></div>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        </article>
+
+{/* END OF UI/UX SECTION */}
+
+        <article className="service">
+        <div className="service__head">
+        <h3>Web Development</h3></div>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        </article>
+
+{/* END OF WEB DEVELOPMENT SECTION */}
+
+
+        <article className="service">
+        <div className="service__head">
+        <h3>Content Creation</h3></div>
+        <ul>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        <li>
+          <HiBadgeCheck className='service__list-icon'/>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </li>
+        </ul>
+        </article>
+
+{/* END OF CONTENT CREATION SECTION */}
+
+      </div>
     </section>
   )
 }
