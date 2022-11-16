58be7ff7f8e0116d0c98ad695d84dcb23c52b5bb
ezravkatz <ezravkatz@gmail.com>
2022-11-15 21:41:44 -0500

    added typist

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,58 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import ME from '../../assets/me2.jpg'
-import HeaderSocials from './HeaderSocials'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import LOGO from "../../assets/logodark5.png";
+import HeaderSocials from "./HeaderSocials";
+import { Container, Row, Col } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import Typist from "react-typist";
 
 const Header = () => {
   return (
-    <header>
-      <div className="container header__container">
-        <h2>Hello, I am</h2>
-        <h1>Ezra Katz</h1>
-        <h2 className='text-light'>Front-end Developer</h2>
-        <CTA />
-        <HeaderSocials />
+    <header className="header" id="home">
+      <Container>
+        <div className="container header__container">
+          <Row className="align-items-center">
+            <Col xs={12} md={6} xl={7}>
+              <Typist>
+                <span className="first__sentence">
+                  <h2>Hello, my name is </h2>
+                </span>
+                <Typist.Delay ms={500} />
+                <div className="name__container">
+                  <h1> Ezra Katz </h1>
+                </div>
+                <Typist.Delay ms={500} />
+                <h2>Front-End Developer</h2>
+                <Typist.Backspace count={19} delay={500} />
+                <Typist.Delay ms={500} />
+                <h2>UI/UX Designer</h2>
+                <Typist.Backspace count={14} delay={500} />
+                <Typist.Delay ms={500} />
+                <h2>Web Developer</h2>
+                <Typist.Backspace count={13} delay={500} />
+                <Typist.Delay ms={500} />
+                <h2>Copywriter</h2>
+                <Typist.Backspace count={10} delay={500} />
+                <Typist.Delay ms={500} />
+                <h2>Content Creator</h2>
+                <Typist.Backspace count={15} delay={500} />
+              </Typist>
+              <CTA />
+              <HeaderSocials />
+            </Col>
+            <Col xs={12} md={6} xl={5}>
+              <div className="me">
+                <img src={LOGO} alt="Ezra Katz Header logo" />
+              </div>
 
-        <div className="me">
-          {/* <img src={ME} alt="A photograph of Ezra Katz in a museum." /> */}
-          <p>Welcome</p>
+              {/* <a href="#contact" className='scroll__down'>Scroll Down</a> */}
+            </Col>
+          </Row>
         </div>
-
-        <a href="#contact" className='scroll__down'>Scroll Down</a>
-      </div>
+      </Container>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/components/header/test.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Typist from "react-typist";
+
+const test = () => {
+  return (
+    <div>
+      <Typist>
+        <h1 style={{ color: "yellow" }}>Front-End Dev</h1>
+      </Typist>
+    </div>
+  );
+};
+
+export default test;
