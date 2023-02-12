4dea421d37966c683c688f20011c1430b3826bda
ezravkatz <ezravkatz@gmail.com>
2023-02-12 10:01:41 -0500

    simplifying header styling and media queries

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import CV from "../../assets/evk-cv-january-2023.pdf";
+import "./header.css";
 
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download className="btn">
-        Download CV
-      </a>
-      <a href="#contact" className="btn btn-primary">
-        Contact
-      </a>
+      <div className="cta__container">
+        <a href={CV} download className="btn">
+          Download CV
+        </a>
+        <a href="#contact" className="btn btn-primary">
+          Contact
+        </a>
+      </div>
     </div>
   );
 };
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,30 +3,26 @@ import "./header.css";
 import CTA from "./CTA";
 import HeaderSocials from "./HeaderSocials";
 import logo from "../../assets/logolight5.png";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Greeting from "./Greeting";
 
 function Header() {
   return (
     <header>
-      <Container>
-        <Row>
-          <Col lg={6} md="auto" xs="auto">
-            <CTA />
-          </Col>
-          <Col lg={3} md="auto" xs="auto">
-            <Greeting />
-          </Col>
-          <Col lg={3} md="auto" xs="auto">
-            <div className="logo">
-              <img src={logo} alt="Ezra Katz Header logo" />
-            </div>
-            <HeaderSocials />
-          </Col>
-        </Row>
-      </Container>
+      <div className="header__container">
+        <div className="cta">
+          <CTA />
+        </div>
+        <div className="greeting__container">
+          <Greeting />
+        </div>
+
+        <div className="logo__container">
+          <div className="logo">
+            <img src={logo} alt="Ezra Katz Header logo" />
+          </div>
+        </div>
+        <HeaderSocials />
+      </div>
     </header>
   );
 }
